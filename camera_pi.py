6a43dd090201f93959876b1ecd5ebb8e49188c20
gagame <30991525+gagame@users.noreply.github.com>
2018-10-06 13:45:56 +0800

    Update camera_pi.py

--- camera_pi.py
+++ camera_pi.py
@@ -1,16 +1,3 @@
-#!/usr/bin/python
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-#|R|a|s|p|b|e|r|r|y|P|i|.|c|o|m|.|t|w|
-#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-# Copyright (c) 2014, raspberrypi.com.tw
-# All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-#
-# Author : sosorry
-# Date   : 05/31/2015
-# Origin : http://blog.miguelgrinberg.com/post/video-streaming-with-flask
-
 import cv2
 
 class Camera(object):
